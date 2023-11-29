72d514e01a12432156f588efe20d8c4cfd86352f
juni-crypto <chander.cia005@gmail.com>
2023-11-30 02:04:54 +0530

    inital-commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,13 +5,26 @@ import "../styles/globals.css";
 // This is the chain your dApp will work on.
 // Change this to the chain your app is built for.
 // You can also import additional chains from `@thirdweb-dev/chains` and pass them directly.
-const activeChain = "ethereum";
+// const activeChain = "Testnetdefi";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThirdwebProvider
       clientId={process.env.NEXT_PUBLIC_TEMPLATE_CLIENT_ID}
-      activeChain={activeChain}
+      activeChain={{
+        chainId: 1131,
+        rpc: ["https://eth.testnet.ocean.jellyfishsdk.com"],
+        nativeCurrency: {
+          decimals: 18,
+          name: "DFI",
+          symbol: "DFI",
+        },
+        shortName: "defi", // Display value shown in the wallet UI
+        slug: "defi", // Display value shown in the wallet UI
+        testnet: true, // Boolean indicating whether the chain is a testnet or mainnet
+        chain: "DefiTestNet", // Name of the network
+        name: "DefiTestNet", // Name of the network
+      }}
     >
       <Component {...pageProps} />
     </ThirdwebProvider>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,105 +1,30 @@
-import { ConnectWallet } from "@thirdweb-dev/react";
+import { ConnectWallet, useContract, useContractRead } from "@thirdweb-dev/react";
 import styles from "../styles/Home.module.css";
-import Image from "next/image";
 import { NextPage } from "next";
+import { CONTRACT_ADDRESS } from "../Constants/addresses";
 
 const Home: NextPage = () => {
+  const {
+    contract 
+  } = useContract(CONTRACT_ADDRESS);
+
+  const {
+    data: owner,
+    isLoading: isOwnerLoading,
+  } = useContractRead(contract, "owner")
+
   return (
     <main className={styles.main}>
       <div className={styles.container}>
-        <div className={styles.header}>
-          <h1 className={styles.title}>
-            Welcome to{" "}
-            <span className={styles.gradientText0}>
-              <a
-                href="https://thirdweb.com/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                thirdweb.
-              </a>
-            </span>
-          </h1>
-
-          <p className={styles.description}>
-            Get started by configuring your desired network in{" "}
-            <code className={styles.code}>src/index.js</code>, then modify the{" "}
-            <code className={styles.code}>src/App.js</code> file!
-          </p>
-
-          <div className={styles.connect}>
-            <ConnectWallet
-              dropdownPosition={{
-                side: "bottom",
-                align: "center",
-              }}
-            />
-          </div>
-        </div>
-
-        <div className={styles.grid}>
-          <a
-            href="https://portal.thirdweb.com/"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              src="/images/portal-preview.png"
-              alt="Placeholder preview of starter"
-              width={300}
-              height={200}
-            />
-            <div className={styles.cardText}>
-              <h2 className={styles.gradientText1}>Portal ➜</h2>
-              <p>
-                Guides, references, and resources that will help you build with
-                thirdweb.
-              </p>
-            </div>
-          </a>
-
-          <a
-            href="https://thirdweb.com/dashboard"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              src="/images/dashboard-preview.png"
-              alt="Placeholder preview of starter"
-              width={300}
-              height={200}
-            />
-            <div className={styles.cardText}>
-              <h2 className={styles.gradientText2}>Dashboard ➜</h2>
-              <p>
-                Deploy, configure, and manage your smart contracts from the
-                dashboard.
-              </p>
-            </div>
-          </a>
-
-          <a
-            href="https://thirdweb.com/templates"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              src="/images/templates-preview.png"
-              alt="Placeholder preview of templates"
-              width={300}
-              height={200}
-            />
-            <div className={styles.cardText}>
-              <h2 className={styles.gradientText3}>Templates ➜</h2>
-              <p>
-                Discover and clone template projects showcasing thirdweb
-                features.
-              </p>
-            </div>
-          </a>
+        <ConnectWallet />
+        <h1>Carbon Emission Credit </h1>
+        <div className={styles.ownercontainer}>
+          {isOwnerLoading ? (
+            <p>Loading...</p>
+          ) : (
+            <p>Owner: {owner}</p>
+          )
+          }
         </div>
       </div>
     </main>
